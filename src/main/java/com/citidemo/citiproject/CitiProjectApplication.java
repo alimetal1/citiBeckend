9acf2043b01c771ba217af47fe5f7e862d7b9051
Alimetal <javierapolet@gmail.com>
2023-08-05 13:35:31 -0400

    hghg

---
+++ src/main/java/com/citidemo/citiproject/CitiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.citidemo.citiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitiProjectApplication.class, args);
+    }
+
+}
