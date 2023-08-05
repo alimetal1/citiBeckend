8c007b64233f08bd97d2dd06833379d632505fb4
Alimetal <javierapolet@gmail.com>
2023-08-05 19:05:00 -0400

    adding controller

--- src/main/java/com/citidemo/citiproject/CitiProjectApplication.java
+++ src/main/java/com/citidemo/citiproject/CitiProjectApplication.java
@@ -2,6 +2,9 @@ package com.citidemo.citiproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class CitiProjectApplication {
@@ -9,5 +12,8 @@ public class CitiProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(CitiProjectApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- src/main/java/com/citidemo/citiproject/config/SwaggerConfig.java
+++ src/main/java/com/citidemo/citiproject/config/SwaggerConfig.java
@@ -18,4 +18,5 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
                 .build();
     }
+
 }
